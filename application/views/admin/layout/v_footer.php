8b05d19d9349639848b67f45768dd1d68fccfe6b
abdul-lambada <engineertekno@gmail.com>
2024-09-11 21:56:40 +0700

    CRUD Mata Pelajaran

--- application/views/admin/layout/v_footer.php
+++ application/views/admin/layout/v_footer.php
@@ -21,6 +21,18 @@
 <!-- Custom Theme JavaScript -->
 <script src="<?= base_url() ?>template/back-end/js/startmin.js"></script>
 
+<!-- DataTables JavaScript -->
+<script src="<?= base_url() ?>template/back-end/js/dataTables/jquery.dataTables.min.js"></script>
+<script src="<?= base_url() ?>template/back-end/js/dataTables/dataTables.bootstrap.min.js"></script>
+
+<script>
+	$(document).ready(function() {
+		$('#dataTables-example').DataTable({
+			responsive: true
+		});
+	});
+</script>
+
 </body>
 
 </html>
